9b95396e4e88d5207cf836c9b93ae114f22ae558
jiheon788 <jiheon788@ajou.ac.kr>
2022-12-14 20:40:36 +0900

    solve sec1-3

---
+++ src/section1/3. 연필개수.js
@@ -0,0 +1,11 @@
+const ONE_DOZEN_PENCILS = 12
+
+function solution(n){
+    if (n % ONE_DOZEN_PENCILS) {
+      return parseInt(n / ONE_DOZEN_PENCILS + 1)
+    }
+    return parseInt(n / ONE_DOZEN_PENCILS)
+}
+
+console.log(solution(25));
+console.log(solution(178));
