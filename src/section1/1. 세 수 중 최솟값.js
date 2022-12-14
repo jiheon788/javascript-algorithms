712aec69dd04726e30517ee1daedef6f00dfe9c6
jiheon788 <jiheon788@ajou.ac.kr>
2022-12-14 20:50:12 +0900

    solve sec1-5

--- src/section1/1. 세 수 중 최솟값.js
+++ src/section1/1. 세 수 중 최솟값.js
@@ -1,7 +1,14 @@
+// 조건문
 function solution(a, b, c){
     let answer = a < b && a < c ? a : b < c ? b : c
     
     return answer;
 }
 
+// 내장함수이용
+function solution2(a, b, c){
+  return Math.min(a, b, c);
+}
+
 console.log(solution(6, 5, 11));
+console.log(solution2(6, 5, 11));
---
+++ src/section1/5. 최솟값 구하기.js
@@ -0,0 +1,6 @@
+function solution(arr){         
+    return Math.min(...arr);
+}
+
+let arr=[5, 7, 1, 3, 2, 9, 11];
+console.log(solution(arr));
