f85a4d9c4ce412e72923d8814a1c7bc82807f8f8
jiheon788 <jiheon788@ajou.ac.kr>
2022-12-14 20:44:13 +0900

    solve sec1-4

--- src/section1/3. 연필개수.js
+++ src/section1/3. 연필개수.js
@@ -8,4 +8,6 @@ function solution(n){
 }
 
 console.log(solution(25));
+console.log(solution(24));
+
 console.log(solution(178));
---
+++ src/section1/4. 1부터 N까지의 합.js
@@ -0,0 +1,11 @@
+function solution(n){
+    let sum = 0;
+    for (let i = 1; i <= n; i ++) {
+      sum += i
+    }
+    
+    return sum;
+}
+
+console.log(solution(6));
+console.log(solution(10));
