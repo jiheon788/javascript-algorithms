96b0ef725d942110a1e834d7dd1ff9ea3e579cc4
jiheon788 <jiheon788@ajou.ac.kr>
2022-12-14 20:30:49 +0900

    solve sec1-2

--- src/section1/2. 삼각형 판별하기.js
+++ src/section1/2. 삼각형 판별하기.js
@@ -1,7 +1,17 @@
+// 세 변의 길이를 줬을 때 길이가 가장 긴 변의 길이는 다른 두 변 길이의 합보다 작아야 삼각형을 그릴 수 있다.
+
 function solution(a, b, c){
-    let answer="YES", max;
+    biggestValue = a > b && a > c ? a : b > c ? b : c
+    sumOfSmallerValue = 0;
+
+    [a, b, c].map(num => {
+      if (num !== biggestValue) {
+        sumOfSmallerValue += num
+      }
+    })
     
-    return answer;
+    return sumOfSmallerValue > biggestValue ? "YES" : "NO"
 }
 
+console.log(solution(6, 7, 11));
 console.log(solution(13, 33, 17));
