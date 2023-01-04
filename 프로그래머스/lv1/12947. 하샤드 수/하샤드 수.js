c7a360eb6bebe4adc2750c3113ee02a10dc975ad
Ji-Heon Park <jiheon788@ajou.ac.kr>
2023-01-04 17:08:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,4 +1,4 @@
 function solution(x) {
-    let sum = (x.toString().split("").map(char => Number(char)).reduce((acc, cur) => acc + cur));
-    return !Boolean(x % sum);
+    let sum = x.toString().split("").reduce((acc, cur) => acc + Number(cur), 0);
+    return !(x % sum);
 }
