de7f96e1d0a4a80bcc7c2e2ed89ead9315710efe
Ji-Heon Park <jiheon788@ajou.ac.kr>
2023-01-21 17:07:32 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,16 +1,11 @@
-const getSum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
-
 function solution(d, budget) {
     var answer = 0;
-    
     d.sort((a, b) => a - b).forEach(item => {
         budget -= item;
-        
         if (budget >= 0) {
             answer += 1;
         }
     })
-    
 
     return answer;
 }
