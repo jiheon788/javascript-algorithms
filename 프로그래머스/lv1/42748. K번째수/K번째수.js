cfe85a7224a0c866dd60795f49e194bd41982202
Ji-Heon Park <jiheon788@ajou.ac.kr>
2023-01-21 21:01:00 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,11 +1,8 @@
 function solution(array, commands) {
-    var answer = [];
+    let answer = [];
     commands.forEach(command => {
-        const i = command[0];
-        const j = command[1];
-        const k = command[2];
-        const splicedArr = array.slice().splice(i - 1, j - i + 1).sort((a, b) => a - b);
-        answer.push(splicedArr[k - 1]);
+        const [i, j, k] = command;
+        answer.push(array.slice().splice(i - 1, j - i + 1).sort((a, b) => a - b)[k - 1]);
     })
     
     return answer;
