8e0e355aa0f46fd004ad319b818f8ce670677342
Ji-Heon Park <jiheon788@ajou.ac.kr>
2023-01-23 22:33:54 +0900

    [level 1] Title: 2016년, Time: 0.10 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,13 @@
+const DAYMAP = {
+    '0': "SUN",
+    '1': "MON",
+    '2': "TUE",
+    '3': "WED",
+    '4': "THU",
+    '5': "FRI",
+    '6': "SAT",
+    
+}
+function solution(a, b) {
+    return DAYMAP[new Date(`2016-${a}-${b}`).getDay()];
+}
