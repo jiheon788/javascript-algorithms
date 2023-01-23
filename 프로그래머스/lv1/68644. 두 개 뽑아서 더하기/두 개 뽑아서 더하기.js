a96349ff7db3c576a3cd03b6606ef77dce9e0924
Ji-Heon Park <jiheon788@ajou.ac.kr>
2023-01-23 22:39:38 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.69 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let answer = [];
+    
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            answer.push(numbers[i] + numbers[j]);
+        }
+    }
+    
+    return [...new Set(answer)].sort((a, b) => a - b);
+}
