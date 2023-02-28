9ae8da3e0e27bc09ce4ff482635e9db7c6d2911b
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-28 19:34:25 +0900

    feat: solve section 4-3

--- 인프런/section4/3. 멘토링.js
+++ 인프런/section4/3. 멘토링.js
@@ -1,20 +1,31 @@
-
 function solution(tests) {
-  let answer = [];
+  let pairs = [];
+  const numOfTest = tests.length;
+  const numOfStudents = tests[0].length;
 
-  for (let i = 0; i < tests.length - 1; i++) {
-    for (let j = 1; i < tests.length; j++) {
-      answer.push()
+  for (let i = 1; i <= numOfStudents; i++) {
+    for (let j = 1; j <= numOfStudents; j++) {
+      let cnt = 0;
+      for (let k = 0; k < numOfTest; k++) {
+        let rankOfI = 0;
+        let rankOfJ = 0;
+        for (let s = 0; s < numOfStudents; s++) {
+          if (tests[k][s] === i) rankOfI = s;
+          if (tests[k][s] === j) rankOfJ = s;
+        }
+        if (rankOfI < rankOfJ) cnt += 1;
+      }
+      if (cnt === numOfTest) pairs.push([i, j]);
     }
   }
 
-  return answer;
+  return pairs.length;
 }
 
 let arr = [
-  [3, 4, 1, 2], 
-  [4, 3, 2, 1], 
-  [3, 1, 4, 2]
+  [3, 4, 1, 2],
+  [4, 3, 2, 1],
+  [3, 1, 4, 2],
 ];
 
 console.log(solution(arr));
