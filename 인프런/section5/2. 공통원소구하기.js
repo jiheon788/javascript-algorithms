8fbcbc1ac4d2432d01eaa5902cfdd4935838254e
jiheon788 <jiheon788@ajou.ac.kr>
2023-03-02 18:27:13 +0900

    feat: solve section 5-2

---
+++ 인프런/section5/2. 공통원소구하기.js
@@ -0,0 +1,7 @@
+function solution(arr1, arr2) {
+  return arr1.filter(item => arr2.includes(item)).sort((a, b) => a - b);
+}
+
+let a = [1, 3, 9, 5, 2];
+let b = [3, 2, 5, 7, 8];
+console.log(solution(a, b));
