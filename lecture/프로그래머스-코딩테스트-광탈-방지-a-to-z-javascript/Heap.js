92972def35909f6f53dc611c0163b4bf0f84974d
Jiheon Park <harrypark.dev@gmail.com>
2024-04-25 18:57:14 +0900

    feat: minheap

--- lecture/프로그래머스-코딩테스트-광탈-방지-a-to-z-javascript/Heap.js
+++ lecture/프로그래머스-코딩테스트-광탈-방지-a-to-z-javascript/Heap.js
@@ -67,3 +67,61 @@ console.log(heap.heap);
 console.log(heap.pop());
 
 console.log(heap.heap);
+
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    this.heap.push(value);
+
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] > value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  pop() {
+    if (this.heap.length < 2) return null; // 힙이 비어있으면 null 반환
+    if (this.heap.length === 2) return this.heap.pop(); // 힙에 원소가 하나만 있을 경우 그 원소 반환
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (
+      this.heap[currentIndex] > this.heap[leftIndex] ||
+      (this.heap[rightIndex] && this.heap[currentIndex] > this.heap[rightIndex])
+    ) {
+      if (this.heap[leftIndex] > this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+
+        currentIndex = rightIndex;
+      } else {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+
+        currentIndex = leftIndex;
+      }
+
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+
+    return returnValue;
+  }
+}
